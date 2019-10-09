e7f90e897a35a3abe7e2d6f07fd6d2fa7d10e1fb
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-09 19:27:24 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,35 @@
+require 'pry'
+
+class Author 
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def add_post(post_title)
+    post_title.author = self
+  end 
+  
+  def add_post_by_title(title)
+    postName = Post.new(title) 
+    add_post(postName)
+    binding.pry
+  end 
+  
+  # def add_song_by_name(song_name)
+   # song = Song.new(song_name)
+    # add_song(song)
+  # end 
+  
+  def posts 
+    Post.all.select {|post| post.author = self}
+  end 
+end
